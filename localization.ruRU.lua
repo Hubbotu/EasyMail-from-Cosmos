3cf51a0e585ce571cfb1e1b582db64c231d61a63
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-11-01 06:12:46 +0300

    Add files via upload

---
+++ localization.ruRU.lua
@@ -0,0 +1,94 @@
+if ( GetLocale() == "ruRU" ) then
+-- Translator ZamestoTV
+EASYMAIL_FORWARDTEXT = "Переслать";
+EASYMAIL_CLEAROPTION = "Очистить поле";
+EASYMAIL_ADDNAMEOPTION = "Добавить имя";
+EASYMAIL_ATTBUTTONTEXT = "Взять всё";
+EASYMAIL_CHECKALLHELPMSG = "Отметить все письма во входящих для получения вложений.";
+EASYMAIL_CLEARALLHELPMSG = "Снять отметки со всех писем во входящих.";
+EASYMAIL_GETALLHELPMSG = "Получить вложения со всех отмеченных писем во входящих.";
+EASYMAIL_CHECKPAGEHELPMSG = "Отметить все письма на текущей странице для получения вложений.";
+EASYMAIL_CLEARPAGEHELPMSG = "Снять отметки со всех писем на текущей странице.";
+EASYMAIL_GUILDTEXT = "Члены гильдии";
+EASYMAIL_FRIENDSTEXT = "Друзья";
+EASYMAIL_RECEIVEMONEY = "Вы получили деньги: %s %s";
+EASYMAIL_FROMAUCTION = "с аукциона %s";
+EASYMAIL_FROM = "от %s";
+EASYMAIL_MONEYSUBJECT = "Деньги: %s";
+EASYMAIL_TOTALMONEY = "Всего получено денег из вложений: %s";
+EASYMAIL_RECEIVEATT = "Вы получили вложение: %s";
+EASYMAIL_NOTEXT = "<Нет текста>";
+EASYMAIL_FORWARD_PREFIX = "ПРД:"
+
+EASYMAIL_CLEARQUESTION = "Очистить всех недавно отправленных адресатов из списка EasyMail?";
+EASYMAIL_ADDNAMETEXT = "Введите имя персонажа для добавления в список адресатов:"
+EASYMAIL_ADDNAMEERR = "Имя '%s' уже есть в списке адресатов."
+EASYMAIL_ADDNAMEMSG = "Имя '%s' добавлено в список адресатов."
+EASYMAIL_DELNAMEQUESTION = "Удалить %s из списка адресатов?";
+EASYMAIL_DELNAMEMSG = "EasyMail удалил %s из списка адресатов.";
+EASYMAIL_DELETEQUESTION = "Удалить письмо от %s?";
+EASYMAIL_DELETEUNREADQUESTION = "Удалить непрочитанное письмо от %s?";
+
+EASYMAIL_CLEARHELPMSG = "Очистить недавно отправленных адресатов из списка адресатов.";
+EASYMAIL_MAXLENHELPMSG = "Изменить максимальную длину списка недавно отправленных адресатов.";
+EASYMAIL_AUTOADDHELPMSG = "Автоматически добавлять вошедших персонажей в список недавно отправленных адресатов.";
+EASYMAIL_GUILDHELPMSG = "Включать имена членов гильдии в выпадающий список адресатов.";
+EASYMAIL_INCLUSIVEHELPMSG = "Полный поиск включает список адресатов, друзей и гильдию в одном списке. По умолчанию — сначала список адресатов, затем друзья, затем гильдия.";
+EASYMAIL_BLIZZLISTHELPMSG = "Использовать стандартный поиск и автозаполнение Blizzard в поле адресата.";
+EASYMAIL_HIDEDROPHELPMSG = "Скрыть выпадающую кнопку адресатов EasyMail.";
+EASYMAIL_OPENALLHELPMSG = "Показывать стандартную кнопку Blizzard «Открыть всё» внизу входящих.";
+EASYMAIL_FRIENDSHELPMSG = "Включать имена друзей в выпадающий список адресатов.";
+EASYMAIL_CLICKGETHELPMSG = "Включить получение вложений из писем во входящих по ПКМ.";
+EASYMAIL_CLICKDELHELPMSG = "Включить удаление писем во входящих по ПКМ.";
+EASYMAIL_DELPROMPTHELPMSG = "Запрашивать подтверждение удаления прочитанных писем при клике ПКМ.";
+EASYMAIL_DELPENDINGHELPMSG = "Удалять ожидающие письма аукциона при получении всех вложений.";
+EASYMAIL_MONEYHELPMSG = "Выводить полученные деньги за каждое письмо в чат.";
+EASYMAIL_TOTALHELPMSG = "Выводить общую сумму денег из всех вложений в чат.";
+EASYMAIL_HISTLENHELPMSG = "Изменить максимальное количество записей в истории отправленных писем. Ноль — без истории.";
+EASYMAIL_TOOLTIPHELPMSG = "Показывать текст письма в подсказке во входящих. ВНИМАНИЕ: письма будут помечены как "
+   .."прочитанные сразу после показа. Текст может появиться с задержкой.";
+
+EASYMAIL_CONFIG_DEFAULTEXT = "Сбросить все настройки EasyMail на стандартные?"
+
+EASYMAIL_CONFIG_MAXLEN = "Максимальная длина списка (%i - %i)";
+EASYMAIL_CONFIG_CLEARLIST = "Очистить список недавно отправленных";
+EASYMAIL_CONFIG_CLEARLISTBTNTEXT = "Очистить список";
+EASYMAIL_CONFIG_AUTOADD = "Добавлять вошедших персонажей в список недавно отправленных";
+EASYMAIL_CONFIG_GUILD = "Добавлять членов гильдии в список адресатов";
+EASYMAIL_CONFIG_INCLUSIVE = "Использовать полный поиск при вводе адресата";
+EASYMAIL_CONFIG_BLIZZLIST = "Использовать стандартный поиск и автозаполнение Blizzard";
+EASYMAIL_CONFIG_HIDEDROP = "Скрыть выпадающую кнопку адресатов EasyMail";
+EASYMAIL_CONFIG_OPENALL = "Показывать кнопку Blizzard «Открыть всё»";
+EASYMAIL_CONFIG_FRIENDS = "Добавлять друзей в список адресатов";
+EASYMAIL_CONFIG_CLICKGET = "Получение вложений по ПКМ";
+EASYMAIL_CONFIG_CLICKDEL = "Удаление писем по ПКМ";
+EASYMAIL_CONFIG_DELPROMPT = "Запрос подтверждения удаления прочитанных писем";
+EASYMAIL_CONFIG_DELPENDING = "Удалять ожидающие письма аукциона";
+EASYMAIL_CONFIG_MONEY = "Выводить полученные деньги за письмо";
+EASYMAIL_CONFIG_TOTAL = "Выводить общую сумму полученных денег";
+EASYMAIL_CONFIG_HISTLEN = "Количество записей в истории";
+EASYMAIL_CONFIG_TOOLTIP = "Показывать текст письма в подсказке во входящих";
+
+EASYMAIL_ERROUTOFRANGE = "Длина списка должна быть от %i до %i.";
+EASYMAIL_ERRTIMEOUT = "Не удалось получить вложение в течение %i секунд. Проверьте место в сумках или уникальные предметы. "
+   .."Если у вас проблемы с задержкой сети, попробуйте повторить процесс позже.";
+
+EASYMAIL_L_MESSAGETO = "Сообщение для";
+EASYMAIL_L_WSUBJ = "с темой";
+EASYMAIL_L_SENT = "отправлено";
+EASYMAIL_L_Message = "Сообщение";
+EASYMAIL_L_MessageShort = "Письмо";
+EASYMAIL_L_To = "Кому:";
+EASYMAIL_L_From = "От:";
+EASYMAIL_L_Subject = "Тема:";
+EASYMAIL_L_LMS = "Последнее отправленное письмо:";
+EASYMAIL_L_HistoryTitle = "EasyMail: история отправленных писем";
+EASYMAIL_L_NoMessages = "Писем не отправлено";
+EASYMAIL_L_OpenList = "Клик — открыть историю отправленных писем.\nПКМ — открыть настройки EasyMail.";
+EASYMAIL_L_OptMoveHFrame = "Сделать окно истории перемещаемым";
+EASYMAIL_L_OptMoveHFrameTT = "Включает и отключает перемещение окна истории. Полезно, если окно ушло за пределы экрана.\n\nЧтобы сбросить — отключите опцию и перезагрузите интерфейс. Окно появится по центру. Затем включите опцию снова.";
+EASYMAIL_L_Date = "Дата:";
+EASYMAIL_L_CONFIG_WRITEOPTIONS = "Применить";
+EASYMAIL_L_CONFIG_WRITEOPTIONST = "Сохранить конфигурацию.";
+EASYMAIL_L_CONFIG_WRITEOPTIONSTT = "Применить изменения и сохранить их.";
+end
